bcbb5c38aa922cce37360b6493986ea77e6fec9d
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-06-05 20:20:38 +0300

    signup update

--- process-signup.php
+++ process-signup.php
@@ -30,5 +30,23 @@ $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
 
-print_r($_POST);   
-var_dump($password_hash);
+$sql = "INSERT INTO user (name,email,password_hash)
+        VALUES (?,?,?)";
+
+$stmt = $mysqli-> stmt_init();
+
+if ( !$stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt-> bind_param("sss", $_POST["name"], $_POST["email"], $password_hash);
+
+if (!$stmt->execute()) {
+    if ($mysqli->errno === 1062) {
+        echo "Same email already exists";
+    } else {
+        die("SQL error: " . $stmt->error . " Error number: " . $mysqli->errno);
+    }  
+} else {
+    echo 'nice job';
+}
