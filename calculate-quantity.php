720b458e122de89107d04b6c1004ce82f8676215
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-06-08 11:22:13 +0300

    one row update func add

--- calculate-quantity.php
+++ calculate-quantity.php
@@ -3,6 +3,8 @@ session_start();
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
+$total = 0;
+
 if (isset($_SESSION['user_id'])) {
     $mysqli = require __DIR__ . "/database.php";
     $sql = "SELECT * FROM user 
@@ -10,28 +12,46 @@ if (isset($_SESSION['user_id'])) {
     $result = $mysqli->query($sql);
     $user = $result->fetch_assoc();
 
-    $total = 0;
+    $basketSqlCall= "SELECT * FROM basket WHERE user_id = {$user["id"]}";
+    $basketResult = $mysqli->query($basketSqlCall);
+    $basket = $basketResult->fetch_assoc();
+    
+    if(isset($basket['user_id'])){
+      $updateTable = "UPDATE basket SET counter_number = {$_POST["quantity"]} WHERE user_id = {$user["id"]}";
+        $updateResult = $mysqli->query($updateTable);
+        $stmt = $mysqli-> stmt_init();
+        if ( !$stmt->prepare($updateTable)) {
+            die("SQL error: " . $mysqli->error);
+        }
+        $stmt-> bind_param("s",$_POST["quantity"]);
+        
+        if(!$stmt->execute()){
+            die("SQL error: " . $stmt->error . " Error number: " . $mysqli->errno);
+        }
+        header("Location: index.php");
+        exit();
+    } else {
+        
     $sql2 = "INSERT INTO basket(user_id, counter_number, total)
-        VALUES (?,?,?)";
+    VALUES (?,?,?)";
     $stmt = $mysqli-> stmt_init();
-
-if ( !$stmt->prepare($sql2)) {
-    die("SQL error: " . $mysqli->error);
-}
-$stmt-> bind_param("sss",$user["id"],$_POST["quantity"], $total);
-
-
-if(!$stmt->execute()){
-    die("SQL error: " . $stmt->error . " Error number: " . $mysqli->errno);
-}
-
-exit();
-
+    if ( !$stmt->prepare($sql2)) {
+        die("SQL error: " . $mysqli->error);
+    }
+    $stmt-> bind_param("sss",$user["id"],$_POST["quantity"], $total);
+    
+    
+    if(!$stmt->execute()){
+        die("SQL error: " . $stmt->error . " Error number: " . $mysqli->errno);
+    }
+    header("Location: index.php");
+    exit();
+    } 
 }else{
     header("Location: login.php");
-    
     exit;
 }
+
 
 
 
