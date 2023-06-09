ae3de7239756a06fd4b6fb57e007114c7e6cc7bc
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-06-09 15:38:06 +0300

    email-verification added

--- email-verification.php
+++ email-verification.php
@@ -1,8 +1,44 @@
-<?php 
+<?php
+session_start();
 ini_set('display_errors', true);
 error_reporting(E_ALL);
-$mysqli = require __DIR__ . "/database.php";
 
+
+if (isset($_SESSION['user_id']) ) {
+    
+    if( !isset($user)){
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = "SELECT * FROM user 
+            WHERE id = {$_SESSION['user_id']}";
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+  
+    }
+    
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+
+    $verification_email = $_POST['verification_email'];
+    $verification_code = $_POST['verification_code'];
+
+    if($verification_code == $user['verification_code'] && $verification_email == $user['email']) {
+        $updateSql = "UPDATE user SET email_verified_at = NOW() WHERE id = {$user['id']}";
+    $result = $mysqli->query($updateSql);
+    $stmt = $mysqli-> stmt_init();
+if (!$stmt->prepare($updateSql)) {
+    die("SQL error: " . $mysqli->error);
+}
+        
+        if(!$stmt->execute()){
+            die("SQL error: " . $stmt->error . " Error number: " . $mysqli->errno);
+        }
+        
+        header("Location: index.php");
+        exit();
+    }
+}
 
 ?>
 
@@ -19,3 +55,13 @@ $mysqli = require __DIR__ . "/database.php";
 
 <body>
     <h1>Verification Page</h1>
+    <form method="post" id="verification">
+
+        <input type="text" name="verification_email" id="verification_email"
+            placeholder=<?=htmlspecialchars($user['email'])?>>
+        <input type="text" name="verification_code" id="verification_code" placeholder="Verification Code?">
+        <input type="submit" name="verify_email" value="Verify Email" </input>
+    </form>
+</body>
+
+</html>
